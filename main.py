e05a353d9c1ab5fe7207aa1b055870a1dcdb7b76
YE GAO <sortsimilar@gmail.com>
2021-03-09 19:45:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 # 1. Corpus;
 def tokenize_corpus(thisCorpus):
@@ -67,6 +68,34 @@ for sentence in tokenized_corpus:
 
 idx_pairs = np.array(idx_pairs) # it will be useful to have this as numpy array
 print("idx_pair: ", idx_pairs)
+
+embedding_dims = 5
+W1 = Variable(torch.randn(embedding_dims, vocabulary_size).float(), requires_grad=True)
+W2 = Variable(torch.randn(vocabulary_size, embedding_dims).float(), requires_grad=True)
+num_epochs = 100
+learning_rate = 0.001
+
+for epo in range(num_epochs):
+    loss_val = 0
+    for data, target in idx_pairs:
+        x = Variable(get_input_layer(data)).float()
+        y_true = Variable(torch.from_numpy(np.array([target])).long())
+
+        z1 = torch.matmul(W1, x)
+        z2 = torch.matmul(W2, z1)
+
+        log_softmax = F.log_softmax(z2, dim=0)
+
+        loss = F.nll_loss(log_softmax.view(1, -1), y_true)
+        loss_val += loss.data[0]
+        loss.backward()
+        W1.data -= learning_rate * W1.grad.data
+        W2.data -= learning_rate * W2.grad.data
+
+        W1.grad.data.zero_()
+        W2.grad.data.zero_()
+    if epo % 10 == 0:
+        print(f'Loss at epo {epo}: {loss_val / len(idx_pairs)}')
 
 
 

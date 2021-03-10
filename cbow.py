ffa1a4098bfc1f0ce2de81876965b3d29ae102c9
YE GAO <sortsimilar@gmail.com>
2021-03-10 09:18:03 +0800

    Update cbow.py

--- cbow.py
+++ cbow.py
@@ -16,12 +16,12 @@ As they evolve, processes manipulate other abstract things called data.
 The evolution of a process is directed by a pattern of rules
 called a program. People create programs to direct processes. In effect,
 we conjure the spirits of the computer with our spells.""".split()
-print("raw_text: ", raw_text)
+print("Raw text: ", raw_text)
 
 # By deriving a set from `raw_text`, we deduplicate the array
 vocab = set(raw_text)
 vocab_size = len(vocab)
-print("vocab: ", vocab)
+print("\nVocabulary: ", vocab)
 
 word_to_ix = {word: ix for ix, word in enumerate(vocab)}
 ix_to_word = {ix: word for ix, word in enumerate(vocab)}

5f92fa162ae416c072a8e1b60f8dbbea6d57e3b9
YE GAO <sortsimilar@gmail.com>
2021-05-21 10:52:00 +0800

    Update skip_gram_ssfc.py

--- skip_gram_ssfc.py
+++ skip_gram_ssfc.py
@@ -20,7 +20,7 @@ def tokenize_sentence(sentences):  # split each sentence into list, made up with
 
 
 # Input layer is just the center word encoded in one-hot manner. It dimensions are [1, vocabulary_size]
-def get_input_layer(word_idx):
+def get_input_layer(word_idx, vocabulary_size):
     x = torch.zeros(vocabulary_size).float()
     x[word_idx] = 1.0
     return x
@@ -137,7 +137,37 @@ learning_rate = 0.001
 for epo in range(num_epochs):
     loss_val = 0
     for data, target in idx_pairs:
-        x = Variable(get_input_layer(data)).float()  # x is a vector, size 15;
+        x = Variable(get_input_layer(data, vocabulary_size)).float()  # x is a vector, size 15;
+        y_true = Variable(torch.from_numpy(np.array([target])).long())
+
+        z1 = torch.matmul(W1, x)  # 2-dimension times 1-dimensions, return 1 dimension;
+        z2 = torch.matmul(W2, z1)  # 2-dimension times 1-dimensions, return 1 dimension;
+
+        log_softmax = F.log_softmax(z2, dim=0)
+
+        loss = F.nll_loss(log_softmax.view(1, -1), y_true)
+        loss_val += loss.item()
+        loss.backward()
+        W1.data -= learning_rate * W1.grad.data
+        W2.data -= learning_rate * W2.grad.data
+
+        W1.grad.data.zero_()
+        W2.grad.data.zero_()
+
+    if epo % 10 == 0:
+        print(f'Loss at epo {epo}: {loss_val / len(idx_pairs)}')
+
+
+embedding_dims = 5
+W1 = Variable(torch.randn(embedding_dims, word_size).float(), requires_grad=True)
+W2 = Variable(torch.randn(word_size, embedding_dims).float(), requires_grad=True)
+num_epochs = 200
+learning_rate = 0.001
+
+for epo in range(num_epochs):
+    loss_val = 0
+    for data, target in word_pairs:
+        x = Variable(get_input_layer(data, word_size)).float()  # x is a vector, size 15;
         y_true = Variable(torch.from_numpy(np.array([target])).long())
 
         z1 = torch.matmul(W1, x)  # 2-dimension times 1-dimensions, return 1 dimension;

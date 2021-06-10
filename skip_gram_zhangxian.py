52d28417a99329f4b15910a8c31b9ec01750f6a5
YE GAO <sortsimilar@gmail.com>
2021-06-10 19:22:44 +0800

    Update skip_gram_zhangxian.py

--- skip_gram_zhangxian.py
+++ skip_gram_zhangxian.py
@@ -25,6 +25,77 @@ DELETE_WORDS = False  # 是否删除部分高频词
 VOCABULARY_SIZE = 10000
 
 
+# DataLoader自动帮忙生成batch
+class WordEmbeddingDataset(tud.Dataset):
+    def __init__(self, data, word_freqs):
+        super(WordEmbeddingDataset, self).__init__()
+        self.data = torch.Tensor(data).long()  # 解码为词表中的索引
+        self.word_freqs = torch.Tensor(word_freqs)  # 词频率
+
+    def __len__(self):
+        # 共有多少个item
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # 根据idx返回
+        center_word = self.data[idx]  # 找到中心词
+        pos_indices = list(range(idx - WINDOW_SIZE, idx)) + list(
+            range(idx + 1, idx + WINDOW_SIZE + 1))  # 中心词前后各C个词作为正样本
+        # pos_indices = list(filter(lambda i: i >= 0 and i < len(self.data), pos_indices))  # 过滤，如果索引超出范围，则丢弃
+        pos_indices = [i % len(self.data) for i in pos_indices]
+        pos_words = self.data[pos_indices]  # 周围单词
+        # 根据 变换后的词频选择 K * 2 * C 个负样本，True 表示可重复采样
+        neg_words = torch.multinomial(self.word_freqs, N_SAMPLES * pos_words.shape[0], True)
+
+        return center_word, pos_words, neg_words
+
+
+# 构造一个神经网络，输入词语，输出词向量
+class EmbeddingModel(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(EmbeddingModel, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.embed_size = embed_size
+
+        initrange = 0.5 / self.embed_size
+        self.out_embed = nn.Embedding(self.vocab_size, self.embed_size, sparse=False)
+        # 模型输出nn.Embedding(30000, 100)
+        self.out_embed.weight.data.uniform_(-initrange, initrange)  # 权重初始化的一种方法
+
+        self.in_embed = nn.Embedding(self.vocab_size, self.embed_size, sparse=False)
+        # 模型输入nn.Embedding(30000, 100)
+        self.in_embed.weight.data.uniform_(-initrange, initrange)
+        # 权重初始化的一种方法
+
+    def forward(self, input_labels, pos_labels, neg_labels):
+        # input_labels:[batch_size]
+        # pos_labels:[batch_size, windows_size*2]
+        # neg_labels:[batch_size, windows_size * N_SAMPLES]
+
+        input_embedding = self.in_embed(input_labels)  # [batch_size, embed_size]
+        pos_embedding = self.out_embed(pos_labels)  # [batch_size, windows_size * 2, embed_size]
+        neg_embedding = self.out_embed(neg_labels)  # [batch_size, (windows_size * 2 * K), embed_size]
+
+        # 向量乘法
+        input_embedding = input_embedding.unsqueeze(2)  # [batch_size, embed_size, 1],新增一个维度用于向量乘法
+        # input_embedding = input_embedding.view(BATCH_SIZE, EMBEDDING_DIM, 1)
+        pos_dot = torch.bmm(pos_embedding, input_embedding).squeeze(2)  # [batch_size, windows_size * 2] 只保留前两维
+        neg_dot = torch.bmm(neg_embedding.neg(), input_embedding).squeeze(
+            2)  # [batch_size, windows_size * 2 * K] 只保留前两维
+
+        log_pos = F.logsigmoid(pos_dot).sum(1)  # 按照公式计算
+        log_neg = F.logsigmoid(neg_dot).sum(1)
+
+        loss = -(log_pos + log_neg)  # [batch_size]
+
+        return loss
+
+    def input_embeddings(self):
+        ##取出self.in_embed数据参数
+        return self.in_embed.weight.data.cpu().numpy()
+
+
 def maybe_download(file_name, expected_bytes, url):
     if not os.path.exists(file_name):
         file_name, _ = urllib.request.urlretrieve(url + file_name, file_name)
@@ -106,77 +177,6 @@ word_freqs = word_freqs / np.sum(word_freqs)
 
 
 # print(word_freqs)
-
-# DataLoader自动帮忙生成batch
-class WordEmbeddingDataset(tud.Dataset):
-    def __init__(self, data, word_freqs):
-        super(WordEmbeddingDataset, self).__init__()
-        self.data = torch.Tensor(data).long()  # 解码为词表中的索引
-        self.word_freqs = torch.Tensor(word_freqs)  # 词频率
-
-    def __len__(self):
-        # 共有多少个item
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        # 根据idx返回
-        center_word = self.data[idx]  # 找到中心词
-        pos_indices = list(range(idx - WINDOW_SIZE, idx)) + list(
-            range(idx + 1, idx + WINDOW_SIZE + 1))  # 中心词前后各C个词作为正样本
-        # pos_indices = list(filter(lambda i: i >= 0 and i < len(self.data), pos_indices))  # 过滤，如果索引超出范围，则丢弃
-        pos_indices = [i % len(self.data) for i in pos_indices]
-        pos_words = self.data[pos_indices]  # 周围单词
-        # 根据 变换后的词频选择 K * 2 * C 个负样本，True 表示可重复采样
-        neg_words = torch.multinomial(self.word_freqs, N_SAMPLES * pos_words.shape[0], True)
-
-        return center_word, pos_words, neg_words
-
-
-# 构造一个神经网络，输入词语，输出词向量
-class EmbeddingModel(nn.Module):
-    def __init__(self, vocab_size, embed_size):
-        super(EmbeddingModel, self).__init__()
-
-        self.vocab_size = vocab_size
-        self.embed_size = embed_size
-
-        initrange = 0.5 / self.embed_size
-        self.out_embed = nn.Embedding(self.vocab_size, self.embed_size, sparse=False)
-        # 模型输出nn.Embedding(30000, 100)
-        self.out_embed.weight.data.uniform_(-initrange, initrange)  # 权重初始化的一种方法
-
-        self.in_embed = nn.Embedding(self.vocab_size, self.embed_size, sparse=False)
-        # 模型输入nn.Embedding(30000, 100)
-        self.in_embed.weight.data.uniform_(-initrange, initrange)
-        # 权重初始化的一种方法
-
-    def forward(self, input_labels, pos_labels, neg_labels):
-        # input_labels:[batch_size]
-        # pos_labels:[batch_size, windows_size*2]
-        # neg_labels:[batch_size, windows_size * N_SAMPLES]
-
-        input_embedding = self.in_embed(input_labels)  # [batch_size, embed_size]
-        pos_embedding = self.out_embed(pos_labels)  # [batch_size, windows_size * 2, embed_size]
-        neg_embedding = self.out_embed(neg_labels)  # [batch_size, (windows_size * 2 * K), embed_size]
-
-        # 向量乘法
-        input_embedding = input_embedding.unsqueeze(2)  # [batch_size, embed_size, 1],新增一个维度用于向量乘法
-        # input_embedding = input_embedding.view(BATCH_SIZE, EMBEDDING_DIM, 1)
-        pos_dot = torch.bmm(pos_embedding, input_embedding).squeeze(2)  # [batch_size, windows_size * 2] 只保留前两维
-        neg_dot = torch.bmm(neg_embedding.neg(), input_embedding).squeeze(
-            2)  # [batch_size, windows_size * 2 * K] 只保留前两维
-
-        log_pos = F.logsigmoid(pos_dot).sum(1)  # 按照公式计算
-        log_neg = F.logsigmoid(neg_dot).sum(1)
-
-        loss = -(log_pos + log_neg)  # [batch_size]
-
-        return loss
-
-    def input_embeddings(self):
-        ##取出self.in_embed数据参数
-        return self.in_embed.weight.data.cpu().numpy()
-
 
 # 构造  dataset 和 dataloader
 dataset = WordEmbeddingDataset(data, word_freqs)
